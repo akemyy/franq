3127eab5d72421d0e39e72efd90fcc4a0ec77847
Patricia Akemy <patricia.akemyy@gmail.com>
2021-09-01 16:00:01 -0300

    conf deploy

--- populate_script.py
+++ populate_script.py
@@ -27,14 +27,18 @@ print('Gerado seed de pessoas')
 
 def criando_veiculo(quantidade_de_veiculo):
     fake = Faker('pt_BR')
+    my_word_list = ['Chevrolet Onix', 'Hyundai HB20', 'Chevrolet Onix Plus', 'Fiat Strada', 'Volkswagen Gol',
+                'Ford Ka', 'Fiat Argo', 'Volkswagen T-Cross', 'Jeep Renegade', 'Fiat Toro',
+                'Jeep Compass', 'Renault Kwid', 'Chevrolet Tracker', 'Hyundai Creta']
+    
     Faker.seed(10)
     for _ in range(quantidade_de_veiculo):
         tipo = random.choice(['M', 'C'])
-        modelo = fake.random_int()
+        modelo = my_word_list[fake.random_int(min=0, max=13)]
         ano_veiculo = fake.random_int()
         cor = fake.color_name()
         placa = fake.random_int()
-        visivel = random.choice([True, False])
+        visivel = random.choice([True, False]) #fake.unique.boolean() 
         p = Veiculo(tipo=tipo, modelo=modelo, ano_veiculo=ano_veiculo, cor= cor, placa=placa, visivel = visivel)
         p.save()
 
